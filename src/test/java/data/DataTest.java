017185c5f63f8c4be3e91995bdcfa3d938079547
sarwargtr <131928874+sarwargtr@users.noreply.github.com>
2023-12-12 23:44:29 -0500

    committed

---
+++ src/test/java/data/DataTest.java
@@ -0,0 +1,27 @@
+package data;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DataTest {
+	
+	
+    WebDriver driver;
+	
+	@Test
+	public void InIt() {
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.get("https://objectspy.space/");
+		driver.manage().window().maximize();
+		// wait
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+}
+	
+
+}
